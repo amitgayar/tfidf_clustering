3f4f42ff005574b41325f4d8a4ba6be4e07d025c
amitgayar <gayaramit@gmail.com>
2019-09-11 18:15:52 +0530

    clust2

--- workflow.py
+++ workflow.py
@@ -1,21 +1,10 @@
 from get_news_links import *
-
-
-
-# keywords = 'mauritius route tax haven'
 keywords = input("Enter keywords :\n")
 get_news_links(keywords)
 
-
+from story_struct import Story_Struct
 from extract_data import *
-from spacy_cleansing import *
-from tfidf import *
 
-print('\n\nEXTRACTION OF DATA-----------------------------------------------------------------------------------------------------------\n')
 extract_data(keywords)
-print('\nSPACY -----------------------------------------------------------------------------------------------------------\n')
-all_doc_list = run_spacy_cleansing(keywords)
-print('\n\nTFIDF_MATRIX-----------------------------------------------------------------------------------------------------------')
-all_doc_list = run_TFIDF(keywords, None)
 
-print('\n\nWORKFLOW COMPLETED\n')
+story.process(step='all', level='articles', no_of_doc=None, compute_again=True, filesave=True)
